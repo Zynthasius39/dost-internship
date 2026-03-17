849ddd519d80ffa58db86b2e52f1a1fd2d9c9665
Zynthasius39 <super.alekberov@protonmail.com>
2026-03-18 03:29:30 +0400

    fix: use command virt-install instead of community.libvirt.virt_install

--- task-3/vars/nodes-jellybean.yml.d
+++ task-3/vars/nodes-jellybean.yml.d
@@ -1,36 +1,36 @@
 nodes:
   - name: k8s-eclair-1
-    ip: 192.168.122.11
+    ip: 10.0.16.11
     mac: "52:54:00:0a:10:0b"
-    memory: 2048
+    memory: 4096
     vcpu: 2
     role: master
 
   - name: k8s-eclair-2
-    ip: 192.168.122.12
+    ip: 10.0.16.12
     mac: "52:54:00:0a:10:0c"
-    memory: 4096
+    memory: 8192
     vcpu: 4
     role: worker
 
   - name: k8s-eclair-3
-    ip: 192.168.122.13
+    ip: 10.0.16.13
     mac: "52:54:00:0a:10:0d"
-    memory: 4096
+    memory: 8192
     vcpu: 4
     role: worker
-
+ 
 cidr: /24
-gateway: 192.168.122.1
+gateway: 10.0.16.1
 nameservers:
   search:
     - alak
   addresses:
-    - 192.168.122.1
+    - 10.0.16.1
 
 os_name: debian13
 vol_capacity: 10
-base_image: /home/zynf/Documents/ISO/debian-13-genericcloud-amd64.qcow2
+base_image: /home/qemu/images/debian-13-genericcloud-amd64.qcow2
 
 ssh_public_key: "{{ 'keys/k8s-eclair.pub' | realpath }}"
 ssh_private_key: "{{ 'keys/k8s-eclair.key' | realpath }}"
